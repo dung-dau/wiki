9cbcd6e1a544e972ba8ce689b8ecac0cfc663e55
Danny Dau <dudau0924222@gmail.com>
2023-05-28 08:38:56 -0700

    finish basic navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { Page } from 'src/app/model/page.model';
+import { PageService } from 'src/app/services/page.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  @ViewChild('burger') span: any;
+
+  pages: Page[] = [];
+  showMobileContents: Boolean = true;
+
+  constructor(private pageService: PageService) {}
+
+  ngOnInit(): void {
+    this.pageService.getAllPages().subscribe(
+      {
+        next: (v: Page) => this.pages.push(v),
+        error: (e: Error) => console.error(e),
+        complete: () => console.info('complete') 
+      }
+    )
+    console.log(this.pages);
+  }
+
+  showContents(): void {
+    this.showMobileContents = !this.showMobileContents;
+    console.log(this.showMobileContents)
+  }
 
 }
